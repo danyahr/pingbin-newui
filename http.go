9fb6ac9b2186f88173a03f05886b5fbf64164a9d
danyahr <41299953+danyahr@users.noreply.github.com>
2022-10-26 17:11:23 +0300

    Update http.go

--- http.go
+++ http.go
@@ -19,14 +19,11 @@ func generateToken() string {
 
 var historyPathRe = regexp.MustCompile(`^/([a-fA-F0-9]{28})/history$`)
 var tokenPathRe = regexp.MustCompile(`^/([a-fA-F0-9]{28})$`)
-var pathPingRe = regexp.MustCompile(`^/p/([a-fA-F0-9]{28})$`)
+var pathPingRe = regexp.MustCompile(`^/p/([a-fA-F0-9]{28})(/|$)`)
 
 func Http(listen string) (<-chan Record, error) {
 	ret := make(chan Record)
-	sockio, err := socketio.NewServer(nil)
-	if err != nil {
-		return nil, err
-	}
+	sockio := socketio.NewServer(nil)
 	sockio.OnEvent("/", "subscribe", func(s socketio.Conn, topic string) {
 		events := Subscribe(topic)
 		sockio.OnDisconnect("/", func(s socketio.Conn, msg string) {
@@ -111,6 +108,8 @@ func Http(listen string) (<-chan Record, error) {
 		}
 	})
 	go func() {
+		go sockio.Serve()
+		defer sockio.Close()
 		err := http.ListenAndServe(listen, nil)
 		if err != nil {
 			log.Fatal(err)
